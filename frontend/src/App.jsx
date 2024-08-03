109b3c7eeee6b6f2f7cc2513d31546e30941f80b
David Changea <14ir155@gmail.com>
2024-08-03 21:12:31 +0200

    debut header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,19 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Header from "./components/Header";
+
 const App = () => {
-  return <div></div>;
+  const basename = import.meta.env.MODE === "production" ? "/" : "";
+  return (
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route index path="/" element={<HomePage />} />
+        </Routes>
+      </Router>
+    </>
+  );
 };
 
 export default App;
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,49 @@
+import { Link } from "react-router-dom";
+import data from "../data/navigation.json";
+import { useState } from "react";
+import { FaBars } from "react-icons/fa";
+import { HiX } from "react-icons/hi";
+
+const Header = () => {
+  const [toggleIcon, setToggleIcon] = useState(false);
+
+  const handleToggleIcon = () => {
+    setToggleIcon(!toggleIcon);
+  };
+
+  const handleClick = () => {
+    setToggleIcon(false);
+  };
+
+  return (
+    <header>
+      <nav className="navigation">
+        <div className="navigation_container">
+          <Link to="/" className="navigation_container_logo">
+            <h1>Portfolio</h1>
+          </Link>
+        </div>
+        <ul
+          className={`navigation_container_menu ${toggleIcon ? "active" : ""}`}
+        >
+          {data.map((item, index) => (
+            <li key={"item" + index} className="navigation_container_menu_item">
+              <Link
+                to={item.link}
+                className="navigation_container_menu_item_links"
+                onClick={handleClick}
+              >
+                {item.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <div className="menu_hamburger_icon" onClick={handleToggleIcon}>
+          {toggleIcon ? <HiX size={40} /> : <FaBars size={40} />}
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
